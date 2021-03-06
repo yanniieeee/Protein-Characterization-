ca8de2990a22d86915a8a71d09b5ab41d7bab21d
yanniieeee <79676714+yanniieeee@users.noreply.github.com>
2021-03-05 16:06:51 -0800

    Add files via upload

--- cathode_anode_gen.m
+++ cathode_anode_gen.m
@@ -5,18 +5,39 @@ gel = rectangle('Position',[1 1 14 4],'EdgeColor', 'k','FaceColor', [0.9, 0.9, 0
 cathode = rectangle('Position',[0 1 1 4],'EdgeColor', 'k','FaceColor', [0.9, 0, 0.4]);
 cathode_text = text(0+1/2,1+4/2,'Cathode','Rotation', 90, 'HorizontalAlignment','center');
 
+
 %cathode
 anode = rectangle('Position',[15 1 1 4],'EdgeColor', 'k','FaceColor', [0, 0.8, 1.0]);
 anode_text = text(15+1/2,1+4/2,'Anode','Rotation', 270, 'HorizontalAlignment','center');
 
+%starting wells 
+AA2_well = rectangle('Position',[8 1.75 0.5 0.75], 'EdgeColor', 'k');
+AA1_well = rectangle('Position',[8 3.5 0.5 0.75], 'EdgeColor', 'k');
+
+%AA1
 AA1 = rectangle('Position',[8 3.5 0.5 0.75], 'EdgeColor', 'k', 'FaceColor', [1.0, 1.0, 0.4]);
 AA1_text = text(8+0.5/2,3.5+0.75/2,'AA1','Rotation', 90, 'HorizontalAlignment','center');
+
+
+
 
 %AA2
 AA2 = rectangle('Position',[8 1.75 0.5 0.75], 'EdgeColor', 'k', 'FaceColor', [0.6, 0.6, 1.0]);
 AA2_text = text(8+0.5/2,1.75+0.75/2,'AA2','Rotation', 90, 'HorizontalAlignment','center');
 axis([0 16 -1 6]);
+axis equal
 axis off
+
+%Positive Charge 
+positive_charge = viscircles([0.5 5.5], .35, 'Color', 'k');
+positive_charge_text = text(.5, 5.55,'+', 'HorizontalAlignment','center');
+
+%Negative Charge  
+negative_charge = viscircles([15.5 5.5], .35, 'Color', 'k');
+negative_charge_text = text(15.5, 5.55,'-', 'HorizontalAlignment','center');
+
+
+
 
 pause(0.3)
 
@@ -32,3 +53,4 @@ for i = 0:20
         pause(0.3);
 end
 end
+
