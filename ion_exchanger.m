42f9a517e840d01e2beb9a327edeae51d2e46ebe
yanniieeee <79676714+yanniieeee@users.noreply.github.com>
2021-03-08 15:50:01 -0800

    Add files via upload

--- ion_exchanger.m
+++ ion_exchanger.m
@@ -1,36 +1,39 @@
-function [statement ] = ion_exchanger(AA1_charge, AA2_charge, AA1, AA2)
+function [statement] = ion_exchanger(AA1_charge, AA2_charge, AA1, AA2)
+    
     if ((AA1_charge > 0 && AA2_charge >0) || (AA1_charge < 0 && AA2_charge < 0) || (AA1_charge == AA2_charge))    
-        print(strcat(AA1, " and ", AA2, " cannot be separated by the ion exchanger"));
+        statement = strcat(AA1, " and ", AA2, " cannot be separated by either a cation or anion exchanger.");
+        return
     elseif AA1_charge == 0 
+        AA_elute1 = AA1;
+        AA_stay = AA2;
+        AA_stay_color = [0.6, 0.6, 1.0];
+        x = ".";
         if AA2_charge > 0 
-            statement = strcat("You can separate ", AA1, " and ", AA2, " using a cation exchanger. ", AA1, " will elute first." ); 
             method = "cation";
         else
-            statement = strcat("You can separate ", AA1, " and ", AA2, " using a anion exchanger. ",...
-                AA1, " will elute first." );
             method = "anion";
         end
     elseif AA2_charge == 0
+        AA_elute1 = AA2;
+        AA_stay = AA1;
+        AA_stay_color = [1.0, 1.0, 0.4];
+        x = ".";
         if AA1_charge > 0 
-            statement = strcat("You can separate ", AA1, " and ", AA2, " using a cation exchanger. ",...
-                AA2, " will elute first." );
             method = "cation";
         else
-            statement = strcat("You can separate ", AA1, " and ", AA2, " using a anion exchanger. ",...
-                AA2, " will elute first." );
             method = "anion";
         end
     else 
+        method = "anion or cation";
         if AA1_charge < 0 && AA2_charge > 0
-            statement = strcat("You can separate ", AA1, " and ", AA2, " using a cation or anion exchanger. ",...
-                AA1, " will elute first in a cation exchanger. ", AA2, ...
-                " will elute first in an anion exchanger.");
-            method = "anion and cation";
+            AA_elute1 = AA1;
+            x = strcat(" in a cation exchanger. ", AA2, " will elute first in an anion exchanger.");
         elseif AA1_charge > 0 && AA2_charge < 0 
-           statement = strcat("You can separate ", AA1, " and ", AA2, " using a cation or anion exchanger. ",...
-                AA2, " will elute first in a cation exchanger. ", AA1, ...
-                " will elute first in an anion exchanger.");
+            AA_elute1 = AA2;
+            x = strcat(" in a cation exchanger. ", AA1, " will elute first in an anion exchanger.");
         end
     end
+    statement = strcat("You can separate ", AA1, " and ", AA2, " using a(n) ", method, " exchanger. ")
+    statement = statement + newline + strcat(AA_elute1, " will elute first", x);
     
 end 
