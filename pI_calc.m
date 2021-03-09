10b2d810e22874f2b31f5f8c723cf34c1d5bb7d2
clairelin0825 <79666077+clairelin0825@users.noreply.github.com>
2021-03-09 14:33:01 -0800

    Update pI_calc.m
    commented descriptions for pI_calc function

--- pI_calc.m
+++ pI_calc.m
@@ -1,12 +1,19 @@
 function pI = pI_calc(AA)
+
+% pI_calc is a function that calculates the pI (isoelectric point) of the given amino acid sequence
+
+% Defining amino acids and their pK1s, pKRs, and PK2s in a dictionary
 pKRs = containers.Map(["K", "R", "H", "D", "E", "Y", "C"], [10.54, 12.48,6.04,3.90,4.07,10.46,8.37]);
 pKRs_charge_below = containers.Map(["K", "R", "H", "D", "E", "Y", "C"], [1,1,1,0,0,0,0]);
 pKRs_charge_equal = containers.Map(["K", "R", "H", "D", "E", "Y", "C"], [-1/2,-1/2,1/2,-1/2,-1/2,-1.5,-1/2]);
 pKRs_charge_above = containers.Map(["K", "R", "H", "D", "E", "Y", "C"], [0,0,0,-1,-1,-1,-1]);
 pK1s_carbonyl = containers.Map(["G","A","V","L","I","M","P","F","W","K","R","H","S","T","N","Q","Y","C","D","E"],[2.35,2.35,2.29,2.33,2.32,2.13,1.95,2.20,2.46,2.16,1.82,1.80,2.19,2.09,2.14,2.17,2.20,1.92,1.99,2.10]);
 pK2s_amino_group = containers.Map(["G","A","V","L","I","M","P","F","W","K","R","H","S","T","N","Q","Y","C","D","E"], [9.78,9.87,9.74,9.74,9.76,9.28,10.54,9.31,9.41,9.06,8.99,9.33,9.21,9.10,8.72,9.13,9.21,10.70,9.90,9.47]);
+
 all_pkas = [];
-new_AA = char(AA);
+new_AA = char(AA); % convert string of amino acids into a vector of individual amino acid letters
+
+% looking through the sequence of amino acids for amino acids with pKRs and adding those pKRs to all_pkas[]
 for i = 1:length(new_AA)
 	single_AA = new_AA(i);
     doesExist = isKey(pK1s_carbonyl,single_AA);
@@ -19,10 +26,13 @@ for i = 1:length(new_AA)
     end 
 end  
 
+% creating list of all the pKas that must be considered for the amino acids in the sequence 
 all_pkas = [0, all_pkas, pK1s_carbonyl(single_AA(1)), pK2s_amino_group(single_AA(end))];
-	
+
+% sorting pKas from lowest to highest
 sorted_pkas = sort(all_pkas);
 
+% iterating through all the pKas in the list for the pKa range at which the overall charge of the amino acid is 0, and then using that range to find the pI
 for i = 1:(length(sorted_pkas) - 1)
 	test_pH = (sorted_pkas(i) + sorted_pkas(i+1))/2;
 	if charge_calc(test_pH, AA) == 0
